dcad366c886510ddb5fcb74e0fb9e39d0ea3067d
laraib Ahmad <laraib.ahmad8983@gmail.com>
2025-07-31 14:58:42 +0530

    add data type

---
+++ disk/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let sales = 12345768;
+let course = 'Typescript';
+let is_Submit = true;
+let leval;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let sales =12345768; // this is a number beacuse typeScript know with asigne with number
+let course='Typescript'; // String type use can see when you over the mouse on variable
+let is_published= true; // boolen type 
+let level;// this is not recomemded 
+
+level= 1; //  beacuse of this issue  initialize with number here .
+level = 'a';//beacuse of this issue  there initialize with  string.
+ 
+
